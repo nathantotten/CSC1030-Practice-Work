5d2022ca26a8e47f4ab0ef22c605f42b4bcb9593
Nathan Totten <nathan.james.totten@gmail.com>
2021-03-13 14:38:56 +0000

    More example code

---
+++ Code_Examples/02-mars (design changer)/js/themeChanger.js
@@ -0,0 +1,7 @@
+function makeDark() {
+    document.getElementById('style').setAttribute("href", "css/dark.css");
+}
+
+function makeLight() {
+    document.getElementById('style').setAttribute("href", "css/light.css");
+}
