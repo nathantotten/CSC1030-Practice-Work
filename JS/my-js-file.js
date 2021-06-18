4e7d870cd086b0ff427b3bded0ee645d340b145d
Nathan Totten <nathan.james.totten@gmail.com>
2021-06-18 13:44:49 +0100

    Making good progress with site

---
+++ JS/my-js-file.js
@@ -0,0 +1,48 @@
+const headingElement = document.getElementById("head01");
+const contentDiv = document.getElementById("content-scrollable");
+
+//Refresh the page contents for "About Me".
+function aboutMe() {
+    //Alter the heading.
+    headingElement.innerHTML = "About Me";
+
+    //Create the first paragraph of "About Me" info.
+    var para1 = document.createElement("p");
+
+    
+}
+
+
+//Refresh the page contents for "Skills & Qualities".
+function skillsAndQualities() {
+    //Alter the heading.
+    headingElement.innerHTML = "Skills & Qualities";
+}
+
+
+//Refresh the page contents for "Employment History".
+function employment() {
+    //Alter the heading.
+    headingElement.innerHTML = "Employment History";
+}
+
+
+//Refresh the page contents for "Education".
+function education() {
+    //Alter the heading.
+    headingElement.innerHTML = "Education";
+}
+
+
+//Refresh the page contents for "Hobbies & Interests".
+function interests() {
+    //Alter the heading.
+    headingElement.innerHTML = "Hobbies & Interests";
+}
+
+
+//Refresh the page contents for "Contact Me".
+function contact() {
+    //Alter the heading.
+    headingElement.innerHTML = "Contact Me";
+}
