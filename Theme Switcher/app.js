07475d4459a637f07bd0405efe74ac852df5dd30
Nathan Totten <nathan.james.totten@gmail.com>
2021-07-03 22:28:42 +0100

    Formatting changes to JS - tidy up

--- Theme Switcher/app.js
+++ Theme Switcher/app.js
@@ -5,7 +5,11 @@ const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
+
+
+
 // Apply the cached theme on reload
+
 const theme = localStorage.getItem('theme');
 const isSolar = localStorage.getItem('isSolar');
 
@@ -13,6 +17,9 @@ if (theme) {
     body.classList.add(theme);
     isSolar && body.classList.add('solar');
 }
+
+
+
 
 // Button event handlers
 
